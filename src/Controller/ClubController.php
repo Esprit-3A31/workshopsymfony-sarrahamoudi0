d78345865c6639904ad4876d34b0a5593de208a7
SarraHamoudi0 <sarra.hamoudi@esprit.tn>
2022-10-06 17:39:04 +0200

    prosit4

---
+++ src/Controller/ClubController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+use App\repository\ClubRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ClubController extends AbstractController
+{
+    #[Route('/club', name: 'app_club')]
+    public function index(): Response
+    {
+        return $this->render('club/index.html.twig', [
+            'controller_name' => 'ClubController',
+        ]);
+    }
+    
+#[Route('/clubs',nom:'app_clubs')]
+public function listClub(ClubRepository $repository)
+{
+    $clubs=$repository->findAll();
+    return $this->render("club/clubs.html.twig",
+    array('tabclubs'=>$clubs));
+}
+}
